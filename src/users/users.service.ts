235480a925bf4420d18921dd8f646d37e7615566
HebrinJ <ashram28@gmail.com>
2024-05-03 20:57:38 +0400

    Начало реализации сущности users

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+    create(user: User): User {
+        return user;
+    }
+
+    findOne(): string {
+        return 'user';
+    }
+
+    updateOne() {}
+
+    removeOne() {}
+    
+}
